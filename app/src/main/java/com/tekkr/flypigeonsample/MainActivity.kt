e5ee412c279e021624e6c4f6ea66f9349afcaa95
yogigorle <gorleyogi@gmail.com>
2021-12-29 14:25:35 +0530

    started design

--- app/src/main/java/com/tekkr/flypigeonsample/MainActivity.kt
+++ app/src/main/java/com/tekkr/flypigeonsample/MainActivity.kt
@@ -2,10 +2,24 @@ package com.tekkr.flypigeonsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        vp_flights.adapter = adapter
+
+        TabLayoutMediator(tl_flights, vp_flights) { tab, position ->
+            when (position) {
+                0 -> tab.text = "One Way"
+                1 -> tab.text = "Round Trip"
+                2 -> tab.text = "Multi City"
+            }
+        }.attach()
+
     }
 }
