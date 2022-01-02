82c13cc81c8a58441885a1886b9186e7e6e41f36
yogigorle <gorleyogi@gmail.com>
2022-01-02 20:54:23 +0530

    implemented date picker in one way frag

--- app/src/main/java/com/tekkr/flypigeonsample/fragments/OneWayFragment.kt
+++ app/src/main/java/com/tekkr/flypigeonsample/fragments/OneWayFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.tekkr.flypigeonsample.R
+import kotlinx.android.synthetic.main.fragment_one_way.*
 
 
 class OneWayFragment : Fragment() {
@@ -18,5 +20,19 @@ class OneWayFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_one_way, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //invoke date picker dialog
+        val datePicker = MaterialDatePicker.Builder.datePicker().setTitleText("Select Departure Date").build()
+        datePicker.addOnPositiveButtonClickListener {
+            tv_dep_date.text = datePicker.headerText
+        }
+
+        rl_dep_date_picker.setOnClickListener {
+            datePicker.show(parentFragmentManager,"DEP_DATE_PICKER")
+        }
+    }
+
 
 }
